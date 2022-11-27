bd83445645a8d24200cbac9689e9a89b6fb7f22a
javhidrod8 <javhidrod8@gmail.com>
2022-11-27 12:20:28 +0100

    Formulario para editar y borrar usuarios

---
+++ lib/screens/user_profile.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Perfil de Usuario';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const UserProfile(),
+      ),
+    );
+  }
+}
+
+class UserProfile extends StatefulWidget {
+  const UserProfile({super.key});
+
+  @override
+  UserProfileState createState() {
+    return UserProfileState();
+  }
+}
+
+class UserProfileState extends State<UserProfile> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          // Add TextFormFields and ElevatedButton here.
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Introduzca texto';
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Nombre',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Validate returns true if the form is valid, or false otherwise.
+              if (_formKey.currentState!.validate()) {
+                // If the form is valid, display a snackbar. In the real world,
+                // you'd often call a server or save the information in a database.
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Procesando datos...')),
+                );
+              }
+            },
+            child: const Text('Enviar'),
+          ),
+          ElevatedButton(
+            onPressed: () => showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: const Text('¿Está seguro?'),
+                content: const Text('Esta accion es irreservible'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, 'Cancelar'),
+                    child: const Text('Cancelar'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, 'Confirmar'),
+                    child: const Text('Confirmar'),
+                  ),
+                ],
+              ),
+            ),
+            child: const Text('Eliminar usuario'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget build(BuildContext context) {
+  return TextButton(
+    onPressed: () => showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('AlertDialog Title'),
+        content: const Text('AlertDialog description'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'OK'),
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    ),
+    child: const Text('Show Dialog'),
+  );
+}
